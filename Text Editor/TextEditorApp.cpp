6fa034c4dd89c76bf25c0d34c34ef273656894e7
Mohamed Hussein El-adwy <mohamed.h.eladwy@gmail.com>
2022-04-25 18:01:29 +0200

    the ProtoType and the Main Menu

---
+++ Text Editor/TextEditorApp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+#include "TextEditorFunc.h"
+#include "TextEditorFunc.cpp"
+using namespace std;
+
+int main ()
+{
+    mainMenu();
+    return 0;
+}
---
+++ Text Editor/TextEditorFunc.cpp
@@ -0,0 +1,21 @@
+//____________________________________________________________________________________________
+void mainMenu()
+{
+    cout << "1. Add new text to the end of the file." << endl;
+    cout << "2. Display the content of the file." << endl;
+    cout << "3. Empty the file." << endl;
+    cout << "4. Encrypt the file content." << endl;
+    cout << "5. Decrypt the file content." << endl;
+    cout << "6. Merge another file." << endl;
+    cout << "7. Count the number of words in the file." << endl;
+    cout << "8. Count the number of characters in the file." << endl;
+    cout << "9. Count the number of lines in the file." << endl;
+    cout << "10. Search for a word in the file." << endl;
+    cout << "11. Count the number of times a word exists in the file." << endl;
+    cout << "12. Turn the file content to upper case." << endl;
+    cout << "13. Turn the file content to lower case." << endl;
+    cout << "14. Turn file content to 1st caps (1st char of each word is capital)." << endl;
+    cout << "15. Save." << endl;
+    cout << "16. Exit." << endl;
+}
+//____________________________________________________________________________________________
