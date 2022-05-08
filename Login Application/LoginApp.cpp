d9492afd26059abc7fce6d3f7dfd7c23d68f30de
Mohamed Hussein El-adwy <mohamed.h.eladwy@gmail.com>
2022-05-08 18:57:19 +0200

    Starting the project from Eladwy Guys.

---
+++ Login Application/LoginApp.cpp
@@ -0,0 +1,22 @@
+// FCAI – Programming 1 – 2022 - Assignment 4
+// Program Name: Login Application.
+// Last Modification Date: xx/05/2022
+// Author1 and ID and Group: Mohamed Hussein Hassan Eladwy. ID: 20210335. Group: B. Section: 17 - 18.
+// Author2 and ID and Group: Yusuf Elsayed Abdelrahman Bdr. ID: 20210502. Group: B. Section: 17 - 18.
+// Author3 and ID and Group: Amr Khalied Elsayed Elhennawy. ID: 20210274. Group: B. Section: 17 - 18.
+// Teaching Assistant: Eng.Hagar Ahmed
+// Purpose: .......
+// File: This is the CPP file that contain the main algorithm of the program.
+//___________________________________________________________________________________________________
+#include <iostream>
+#include <regex>
+#include <fstream>
+#include "LoginFunctions.h"
+using namespace std;
+//___________________________________________________________________________________________________
+int main()
+{
+	cout << "Welcome at our Login Application Program." << endl;
+	return 0;
+}
+//___________________________________________________________________________________________________
